96ab86cec2a662a4b13993bafef858d07bbf80ec
MiniMaliZ <gakisomlebu@gmail.com>
2025-02-21 19:51:41 +0700

    Commit Routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM : 2341760110 <br>
+    Nama: Sufyan Dwi Bagaskara';
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID: ' . $id;
+});
+
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
