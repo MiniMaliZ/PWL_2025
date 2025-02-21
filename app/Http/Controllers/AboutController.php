e4e4fb2db08906020580827905e1ab6ea1f3c8a2
MiniMaliZ <gakisomlebu@gmail.com>
2025-02-21 20:33:56 +0700

    Commit Controllers

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about()
+    {
+        return 'NIM : 2341760110 <br>
+                Nama: Sufyan Dwi Bagaskara';
+    }
+}
