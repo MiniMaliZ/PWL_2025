9c0869ccb6d301b1c6f9d5a7373a554b98b8c80a
MiniMaliZ <gakisomlebu@gmail.com>
2025-03-03 08:18:12 +0700

    menambakan POS

---
+++ POS/resources/views/user.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selamat Datang</title>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            text-align: center;
+        }
+        h1 {
+            color: #333;
+            margin: 5px 0;
+        }
+    </style>
+</head>
+<body>
+    <h1>User Profile</h1>
+    <h1>ID: {{ $id }}</h1>
+    <h1>Name: {{ $name }}</h1>
+</body>
+</html>
