0190dff5ddfd3995f35427fa5b3c647742427425
MiniMaliZ <gakisomlebu@gmail.com>
2025-03-10 21:30:27 +0700

    Commit Praktikum 1 & Copy Project PWL POS Week 3

---
+++ Week_4/PWL_POS/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SalesController;
+
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductControlLer :: class, 'foodBeverage' ]);
+    Route:: get('/beauty-health', [ProductControlLer :: class, 'beautyHealth' ]);
+    Route:: get('/home-care', [ProductController :: class, 'homeCare' ]);
+    Route::get('/baby-kid', [ProductControlLer :: class, 'babykid' ]);
+});
+
+Route::get('/sales', [SalesController::class, 'sales']);
+
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
