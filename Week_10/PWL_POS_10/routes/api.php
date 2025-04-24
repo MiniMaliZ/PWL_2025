39654ef5ce1a391626d386974a9ba6cfcd36c862
MiniMaliZ <gakisomlebu@gmail.com>
2025-04-24 13:23:56 +0700

    Praktikum 4

--- Week_10/PWL_POS_10/routes/api.php
+++ Week_10/PWL_POS_10/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,12 @@ Route::post('/logout-api', App\Http\Controllers\Api\LogoutController::class)->na
 Route::middleware('auth:api')->get('/users', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/levels', [LevelController::class, 'index']);
+Route::post('/levels', [LevelController::class, 'store']);
+Route::get('/levels/{level}', [LevelController::class, 'show']);
+Route::put('/levels/{level}', [LevelController::class, 'update']);
+Route::delete('/levels/{level}', [LevelController::class, 'destroy']);
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
